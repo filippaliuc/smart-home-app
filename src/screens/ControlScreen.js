88104a36364437a14aa49b71a28f2ecff713b2f9
Filip <paliucfilip@yahoo.com>
2023-05-26 10:24:14 +0300

    Configured firebase for the project. Created Empty screens

---
+++ src/screens/ControlScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const ControlScreen = () => {
+  return (
+    <View>
+      <Text>ControlScreen</Text>
+    </View>
+  )
+}
+
+export default ControlScreen
+
+const styles = StyleSheet.create({})
